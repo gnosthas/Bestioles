f5e00c57cb25f03931a1d7955f124b09cd5729e1
Loic_L <loic.ledreck@imt-atlantique.net>
2022-03-10 23:53:00 +0100

    Added comportements headers and empty functions in cpp

---
+++ src/comportement.cpp
@@ -0,0 +1,28 @@
+#include "comportement.h"
+
+#include "bestiole.h"
+
+void ComportementGregaire::bougeSelonComportement(Bestiole b)
+{
+    //
+}
+
+void ComportementPeureuse::bougeSelonComportement(Bestiole b)
+{
+    //
+}
+
+void ComportementKamikaze::bougeSelonComportement(Bestiole b)
+{
+    //
+}
+
+void ComportementPrevoyante::bougeSelonComportement(Bestiole b)
+{
+    //
+}
+
+void ComportementPersMultiple::bougeSelonComportement(Bestiole b)
+{
+    //
+}
