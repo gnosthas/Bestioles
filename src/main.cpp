9f722c1d204584cc00a9bd8a904da2c602eb838d
Etienne Sesmat <etienne.sesmat@gmail.com>
2022-03-11 15:03:37 +0100

    modifs main

--- src/main.cpp
+++ src/main.cpp
@@ -13,23 +13,23 @@ using namespace std;
 int main()
 {
 
-   // Aquarium       ecosysteme( 640, 480, 30 );
+   Aquarium       ecosysteme( 640, 480, 30 );
 
-   // for ( int i = 1; i <= 20; ++i )
-   //    ecosysteme.getMilieu().addMember( Bestiole() );
-   // ecosysteme.run();
+   for ( int i = 1; i <= 20; ++i )
+      ecosysteme.getMilieu().addMember( Bestiole() );
+   ecosysteme.run();
 
-   ConcreteCreatorCamouflage creator_camouflage;
-   ConcreteCreatorCarapace creator_carapace;
-   ConcreteCreatorNageoire creator_nageoire;
+   // ConcreteCreatorCamouflage creator_camouflage;
+   // ConcreteCreatorCarapace creator_carapace;
+   // ConcreteCreatorNageoire creator_nageoire;
 
-   Camouflage* test_camouflage = creator_camouflage.createAccessoire();
-   Carapace* test_carapace = creator_carapace.createAccessoire();
-   Nageoire* test_nageoire = creator_nageoire.createAccessoire();
+   // Camouflage* test_camouflage = creator_camouflage.createAccessoire();
+   // Carapace* test_carapace = creator_carapace.createAccessoire();
+   // Nageoire* test_nageoire = creator_nageoire.createAccessoire();
 
-   delete test_camouflage;
-   delete test_carapace;
-   delete test_nageoire;
+   // delete test_camouflage;
+   // delete test_carapace;
+   // delete test_nageoire;
 
    return 0;
 
