e873624792913e08b37bd6e189985c60e124913c
gnost <gnosthas99@gmail.com>
2022-03-11 14:34:20 +0100

    Capteur Factory fonctionnel

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Aquarium.h"
 #include "Milieu.h"
 #include "Bestiole.h"
+#include "Capteurs/concreteCreatorYeux.h"
 
 #include <iostream>
 
@@ -12,9 +13,16 @@ int main()
 
    Aquarium       ecosysteme( 640, 480, 30 );
 
-   for ( int i = 1; i <= 20; ++i )
-      ecosysteme.getMilieu().addMember( Bestiole() );
-   ecosysteme.run();
+   // for ( int i = 1; i <= 20; ++i )
+   //    ecosysteme.getMilieu().addMember( Bestiole() );
+   // ecosysteme.run();
+
+
+   ConcreteCreatorYeux eyez;
+   Yeux* test_eyez = eyez.createCapteur();
+
+
+   delete test_eyez;
 
 
    return 0;
