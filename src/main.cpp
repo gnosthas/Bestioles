2075fedfb055b3f08493e98433a1e6a0aa8bc983
Etienne <etienne.sesmat@gmail.com>
2022-03-17 18:52:51 +0100

    Bestioles dans aquarium créés par la factory

--- src/main.cpp
+++ src/main.cpp
@@ -11,20 +11,16 @@ using namespace std;
 int main()
 {
 
-   // Aquarium       ecosysteme( 640, 480, 30 );
-
-   // for ( int i = 1; i <= 20; ++i ){
-   //    IBestiole* bestiole = new Bestiole();
-   //    ecosysteme.getMilieu().addBestiole(bestiole);
-   //    // delete bestiole;
-   // }
-   // ecosysteme.run();
-
+   Aquarium       ecosysteme( 640, 480, 30 );
    ConcreteCreatorBestiole creator_bestiole;
 
-   Bestiole* bestiole = creator_bestiole.createBestiole();
-   delete bestiole;
+   for ( int i = 1; i <= 20; ++i ){
+      Bestiole* bestiole = creator_bestiole.createBestiole();
+      ecosysteme.getMilieu().addBestiole(bestiole);
+   }
+   ecosysteme.run();
 
+      
    return 0;
 
 }
