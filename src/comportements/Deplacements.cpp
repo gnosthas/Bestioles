a5ee0b40334c00be863746214f1b3ea0ffca05f8
Etienne <etienne.sesmat@gmail.com>
2022-03-18 11:35:35 +0100

    set dans deplacements

--- src/comportements/Deplacements.cpp
+++ src/comportements/Deplacements.cpp
@@ -23,20 +23,20 @@ void bougeNormalement( Bestiole &b, Milieu &m )
     ny = b.getY() + dy + cy;
 
     if ( (nx < 0) || (nx > xLim - 1) ) {
-        b.getOrientation() = M_PI - b.getOrientation();
+        b.setOrientation(M_PI - b.getOrientation());
         b.getCumulX() = 0.;
     }
     else {
-        b.getX() = static_cast<int>( nx );
+        b.setX(static_cast<int>( nx ));
         b.getCumulX() += nx - b.getX();
     }
 
     if ( (ny < 0) || (ny > yLim - 1) ) {
-        b.getOrientation() = -b.getOrientation();
+        b.setOrientation(-b.getOrientation());
         b.getCumulY() = 0.;
     }
     else {
-        b.getY() = static_cast<int>( ny );
+        b.setY(static_cast<int>( ny ));
         b.getCumulY() += ny - b.getY();
     }
 
